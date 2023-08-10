b6ede1db638f9d383df8a5fc805e3a864415282a
kj <kautharjappie34@gmail.com>
2023-08-10 14:11:24 +0100

    basic math functions
    tested in console log

---
+++ calculator.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a ,b){
+    
+}
+
+console.log(add(10, 4));
+console.log(multiply(10, 4));
+console.log(subtract(10, 4));
+console.log(divide(10, 4));
