c254b3f21f9fa6f1e168f2236408bf20ff4082ae
pablomartan <pablomartan@kolabnow.com>
2023-08-11 16:25:06 +0200

    feat: finish main functionality

--- calculator.js
+++ calculator.js
@@ -28,3 +28,83 @@ function operate(operator, a, b) {
     }
 }
 console.log(operate("*", 4, 0))
+
+/**
+ * MAKE THE CALCULATOR WORK!
+ */
+const calculator = document.querySelector('.calc-bg');
+const calculator_output = calculator.getElementsByTagName('output')[0];
+
+const operators = ['+', '-', 'x', '/'];
+
+let output = 0,
+    userInput = '0',
+    lastResult = 0,
+    operator;
+
+updateOutput();
+
+function updateOutput() {
+    calculator_output.innerText = output;
+}
+
+function clearCalculator() {
+    output = 0;
+    userInput = '0';
+    lastResult = 0;
+    operator = '';
+}
+
+function backSpace() {
+    userInput = userInput.slice(0, userInput.length - 1);
+    
+    if (!userInput) {
+        userInput = '0';
+    }
+
+    output = userInput;
+}
+
+function numberInput(num) {
+    if (userInput === '0' || operators.includes(userInput)) {
+        userInput = num;
+    } else {
+        userInput += num;
+    }
+
+    output = userInput;
+}
+
+function periodInput() {
+    // if the input includes anything that is not a number, do nothing
+    if (userInput.match(/^\d+$/)) {
+        userInput += '.';
+    }
+
+    output = userInput;
+}
+
+function operatorInput(newOperator) {
+    if (!operator) {
+        operator = newOperator;
+        lastResult = userInput;
+        userInput = '0';
+    } else {
+        let operation = operate(operator, Number(lastResult), Number(userInput));
+        if (operation) {
+            lastResult = operation;
+            output = lastResult;
+        }
+        operator = newOperator;
+        userInput = '0';
+    }
+    output = operator;
+}
+
+function equals() {
+    lastResult = operate(operator, Number(lastResult), Number(userInput));
+    if (lastResult) {
+        output = lastResult;
+        userInput = '0';
+    }
+}
